b5fdb043cc9de3a305333cc3dc4e31cbc9e18d43
Benjamin Morel <benjamin.morel@students.hevs.ch>
2023-05-06 20:19:55 +0200

    Test gif

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import giphy from './giphy.gif'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Amogus</h1>j
+        <img src={giphy}/>
         </header>
     </div>
   );
